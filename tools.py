08c7f82229e69b5cb9ca95d88e68c702d454ad48
LeonardoESousa <leonardo.sousa137@gmail.com>
2022-01-10 19:44:12 +0100

    Criei arquivo novo

---
+++ tools.py
@@ -0,0 +1,11 @@
+## Funcoes
+
+## 1) Pega ultima geometria do log (printa a ultima geometria)
+
+## 2) Pega as frequencias (retorna lista de frequencias)
+
+## 3) Pega (ultimas) energias e forcas de oscilador (duas listas)
+
+## 4) Pega funcional e base usados no calculo (Com IOPs se tiver) (retornar uma string)
+
+## 5) Pega o transition dipole moment do S1 (retorna um numero)
